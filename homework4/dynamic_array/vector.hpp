d7fb74b0b4588badc1232bddc8607b90554e7b54
VardGH <varduhi.ghukasy@gmail.com>
2023-08-20 13:48:56 +0400

    added time complexity for the vector

--- homework4/dynamic_array/vector.hpp
+++ homework4/dynamic_array/vector.hpp
@@ -24,20 +24,47 @@ public:
     int get_size() const;
     int get_capacity() const;
 
-    void push_back(const T& value); // Adds an element at the end
-    void insert(const int pos, const T& value); // Inserts an element at a specific position
-    void insert(const int pos, const T& value, int count); // Inserts multiple elements at a position
-    void insert(const int pos, const vector<T>& other); // Inserts the elements of another vector at a position
-    void insert(const int pos, std::initializer_list<T> l); // Inserts elements from an initializer list
-    void pop_back(); // Removes the last element
-    void remove(const int pos); // Removes an element at a specific position
-    void remove(const int pos, int count); // Removes multiple elements starting from a position
-    void shrink_to_fit(); // Reduces the capacity to match the size
-    void clear(); // Removes all elements
-    bool is_empty(); // Checks if the vector is empty
-    void resize(const int count, const T& value = 0); // Resizes the vector to a specified size
-    T& operator[](const int pos); // Access element at a specific position
-    const T& operator[](const int pos) const; // Const version of the above
+    // Adds an element at the end
+    void push_back(const T& value); // Amortized O(1)
+
+    // Inserts an element at a specific position
+    void insert(const int pos, const T& value); // O(n) 
+
+    // Inserts multiple elements at a position
+    void insert(const int pos, const T& value, int count); // O(n + m)
+
+    // Inserts the elements of another vector at a position
+    void insert(const int pos, const vector<T>& other); // O(n + m)
+
+    // Inserts elements from an initializer list
+    void insert(const int pos, std::initializer_list<T> l); // O(n + m)
+
+    // Removes the last element
+    void pop_back(); // O(1)
+
+    // Removes an element at a specific position
+    void remove(const int pos); // O(n)
+
+    // Removes multiple elements starting from a position
+    void remove(const int pos, int count); // O(n)
+
+    // Reduces the capacity to match the size
+    void shrink_to_fit(); // O(n)
+
+    // Removes all elements
+    void clear(); // O(n)
+
+    // Checks if the vector is empty
+    bool is_empty(); // O(1)
+
+    // Resizes the vector to a specified size
+    void resize(const int count, const T& value = 0); // O(n)
+
+    // Access element at a specific position
+    T& operator[](const int pos); // O(1)
+
+    // Const version of the above
+    const T& operator[](const int pos) const; // O(1)
 
 private:
     T* m_arr; // Pointer to the dynamic array
